f0ba635b2b56530fbaf9261196701eac3535d8ea
AllaAlmgalsh <67188444+AllaAlmgalsh@users.noreply.github.com>
2022-10-17 18:48:32 -0700

    Update cons.dart

--- flukepro/lib/components/cons.dart
+++ flukepro/lib/components/cons.dart
@@ -1,7 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-TextStyle conOnboardingText = TextStyle(
-    fontFamily: 'Cairo',
-    fontSize: 16,
-    color: Colors.white,
-    fontWeight: FontWeight.w200);
+class cons {
+  TextStyle conOnboardingText = TextStyle(
+      fontFamily: 'Cairo',
+      fontSize: 16,
+      color: Colors.white,
+      fontWeight: FontWeight.w200);
+  Color blue = Color(0x0165FC);
+
+  Color Red = Color(0xFF000D);
+}
