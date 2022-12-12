df5bde94985210144e92a785165fec2a55644d14
AllaAlmgalsh <67188444+AllaAlmgalsh@users.noreply.github.com>
2022-12-12 13:17:41 -0800

    home visitor

---
+++ flukepro/lib/screens/visitorScreens/vHome.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/src/widgets/container.dart';
+
+class Vhome extends StatelessWidget {
+  const Vhome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
