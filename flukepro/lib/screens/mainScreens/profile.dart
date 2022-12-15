3deabbbec9126da3710285b483f6ba34a04ed571
AllaAlmgalsh <67188444+AllaAlmgalsh@users.noreply.github.com>
2022-12-15 12:35:01 -0800

    profile

--- flukepro/lib/screens/mainScreens/profile.dart
+++ flukepro/lib/screens/mainScreens/profile.dart
@@ -1,66 +1,17 @@
 import 'package:flukepro/components/cons.dart';
 import 'package:flutter/material.dart';
 import '../../components/bottomNav.dart';
-<<<<<<< Updated upstream
+
 import '../../components/cons.dart';
-=======
+
 import '../../components/visitorEventprev.dart';
 import '../../utils/fireStoreQueries.dart';
 import '../OrganizersScreens/Sections/ongoingEvents.dart';
->>>>>>> Stashed changes
 
 class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-<<<<<<< Updated upstream
-        child: SingleChildScrollView(
-            reverse: true,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CircleAvatar(
-                  //Avatar
-                  backgroundColor: Color(0xff).withOpacity(0),
-                  radius: 50,
-                  child: Image.asset(
-                    'images/avatar.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                            // _auth.currentUser != null
-                            //     ? _auth.currentUser!.email
-                            //         .toString()
-                            //         .split('@')
-                            //         .first
-                            //     :
-                            'الإسم',
-                            style: conHeadingsStyle.copyWith(
-                                fontSize: 16, color: Color(0xFF9ec5e9))),
-                        SizedBox(width: 10),
-                      ],
-                    ),
-                    Text(
-                      //Email
-                      // _auth.currentUser!.email.toString(),
-                      'نوع لاحساب',
-                      style: conHeadingsStyle.copyWith(
-                        fontSize: 14,
-                        color: Color(0xFF9ec5e9).withOpacity(0.8),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            )));
-=======
       child: SingleChildScrollView(
         // reverse: true,
         child: Container(
@@ -153,6 +104,5 @@ class profile extends StatelessWidget {
         ),
       ),
     );
->>>>>>> Stashed changes
   }
 }
